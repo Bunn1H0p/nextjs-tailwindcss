2b10942ef3470d93d0e578517abaf1aa49fdd50c
h.Delta <homosapien.delta@gmail.com>
2025-06-11 13:15:15 +0700

    feature: add static and relative div, images of mountains

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,85 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-screen max-w-screen p-10 bg-slate-900 text-gray-400">With static positioning
+      <div className="mt-5 h-70 max-w-auto bg-slate-700 rounded-xl p-5 text-white">Relative parent
+        <div className=" mt-3 h-50 max-w-auto bg-slate-600 p-5 text-gray-300">Static parent
+          <div className="mt-5 flex gap-3">
+            <div className="w-32 h-18 rounded-xl bg-blue-600 text-center place-content-center">Static child?</div>
+            <div className="md:absolute md:top-0 md:right-0 w-32 h-18 rounded-xl bg-white text-blue-600 text-center place-content-center">Static child?</div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      {/* <div className="columns-2 sm:columns-3 sm:gap-8">
+      <div className="grid gap-x-4 gap-y-4">
+        <div className="aspect-square bg-amber-500">01</div>
+        <div className="aspect-square bg-amber-600">02</div>
+        <Image 
+          src="/mountains-1.png"
+          alt="Mountains 1"
+          width={3456/15}
+          height={5184/15} 
+          className="aspect-portrait"
+        />
+        <Image 
+          src="/mountains-2.png"
+          alt="Mountains 2"
+          width={3714/15}
+          height={5564/15}
+          className="aspect-portrait"
+        />
+        <Image 
+          src="/mountains-3.png"
+          alt="Mountains 3"
+          width={5184/15}
+          height={3456/15}
+          className="aspect-retro"
+        />
+        <Image 
+          src="/mountains-4.png"
+          alt="Mountains 4"
+          width={4096/15}
+          height={2733/15}
+          className="aspect-retro"
+        />
+        <Image 
+          src="/mountains-5.png"
+          alt="Mountains 5"
+          width={5472/15}
+          height={3648/15}
+          className="aspect-retro"
+        />
+        <Image 
+          src="/mountains-6.png"
+          alt="Mountains 6"
+          width={4240/15}
+          height={2832/15}
+          className="aspect-square"
+        />
+        <Image 
+          src="/mountains-7.png"
+          alt="Mountains 7"
+          width={3072/15}
+          height={4608/15}
+          className="aspect-portrait"
+        />
+        <Image 
+          src="/mountains-8.png"
+          alt="Mountains 8"
+          width={2640/15}
+          height={3960/15}
+          className="aspect-portrait"
+        />
+        <Image 
+          src="/mountains-9.png"
+          alt="Mountains 9"
+          width={4272/15}
+          height={2848/15}
+          className="aspect-retro"
+        />
+    </div>
+    </div> */}
     </div>
   );
 }
